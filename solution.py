3c9daf23eaaeb0fc32cc57a565f28a197fc7de6a
pavxl <76624639+pavxl@users.noreply.github.com>
2023-04-13 15:13:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import numpy as np
-from statsmodels.stats.proportion import proportions_ztest
+import numpy
+import statsmodels.stats.proportion as p
+from scipy.stats import chi2
+from scipy.stats import laplace, norm
 
 chat_id = 362844815
 
@@ -8,9 +9,7 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.1
-    _, p_value = proportions_ztest(
-        [x_success, y_success], [x_cnt, y_cnt], alternative="smaller"
-    )
+    v = (x_success / x_cnt - y_success / y_cnt) * numpy.sqrt((x_cnt + y_cnt) * x_cnt * y_cnt / ((x_success + y_success) * (x_cnt + y_cnt - x_success - y_success)))
+    q = norm.ppf(0.1)
     
-    return p_value <= alpha
+    return v <= q
